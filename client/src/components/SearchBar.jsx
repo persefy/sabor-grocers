b74bc12eab369c8f5a461cde53e27732f1d467fc
Stephanie Cancel <stephaniejcancel@gmail.com>
2024-04-09 14:27:44 -0400

    added search bar base code

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,8 +1,19 @@
+import searchIcon from '../assets/searchIcon.png'
 export default function SearchBar() {
+    
     return(
         <>
-        <input type="text"/>
-        <button>Search</button>
+        <form className="search-form">
+            <input type="text" placeholder='search...' className='search-input'
+                onClick={()=> {
+                    document.querySelector('.search-input').style.width = '50%'
+            }}/>
+            <button>
+                
+                {/* <span className='search-btn-text'> search</span> */}
+                <img src={searchIcon} alt='search icon'/>
+            </button>
+        </form>
         </>
     )
 }
