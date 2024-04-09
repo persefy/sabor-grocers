46f3054c82bcf48c586eca4e44f8d17853ec9404
Stephanie Cancel <stephaniejcancel@gmail.com>
2024-04-09 13:51:43 -0400

    added header styling and logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,20 @@
 import { useNavigate } from "react-router-dom"
 import Nav from './Nav'
 import SearchBar from "./SearchBar"
+import logo from '../assets/logo.png'
 
 export default function Header() {
     let navigate = useNavigate()
 
     return(
         <>
-        <h1 onClick={()=> {navigate('/')}}>Sabor Grocers</h1>
-        <Nav/>
-        <SearchBar/>
+        <header>
+            <div className="h1-container">
+                <h1 onClick={()=> {navigate('/')}}><img src={logo} alt='sabor grocers logo' className='logo'></img><span>Sabor Grocers</span></h1>
+            </div>
+            <Nav/>
+            <SearchBar/>
+        </header>
         </>
     )
 }
