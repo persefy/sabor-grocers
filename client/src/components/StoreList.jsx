720e3fc260abdef8d80c9796d2539365a644b619
Stephanie Cancel <stephaniejcancel@gmail.com>
2024-04-10 02:24:44 -0400

    added initial code for store list page

--- client/src/components/StoreList.jsx
+++ client/src/components/StoreList.jsx
@@ -1,7 +1,23 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
 export default function StoreList() {
+
+    const { id } = useParams()
+    const [store, setStore] = useState()
+
+    useEffect(() => {
+        const getStore = async() => {
+            const response = await axios.get(`http://localhost:8000/stores/${id}`)
+            setStore(response.data)
+        }
+        getStore()
+    }, [])
     return(
         <>
+
         {/* Customer facing view of all pages */}
+
         </>
     )
 }
