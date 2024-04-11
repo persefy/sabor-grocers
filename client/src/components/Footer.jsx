a08bdc02e135133826c32620f016d3583076ffbe
Stephanie Cancel <stephaniejcancel@gmail.com>
2024-04-10 21:38:33 -0400

    styled footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,17 @@
-import { Link } from 'react-router-dom'
-
+import { useNavigate } from "react-router-dom"
+import { Button,Icon } from 'semantic-ui-react'
 export default function Footer() {
+    let navigate = useNavigate()
     return(
         <>
         <footer>
-            <div>Footer will go here</div>
-            <Link className='button' to='/stores'>See Stores</Link>
+            <div className='footer-content'>
+
+                {/* <Link className='button' to='/stores'>See Stores</Link> */}
+                <Button className='gray' size="massive" onClick={()=>{navigate('/')}}>See Stores</Button>
+                <p className="credit">Designed and Developed by <a href="https://github.com/persefy" target="_blank"><Icon name='github icon'/>Stephanie Cancel</a></p>
+                <p className="copy">&copy; 2024</p>
+            </div>
         </footer>
         </>
     )
